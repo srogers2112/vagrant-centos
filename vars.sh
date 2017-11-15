01ce3f6c8abee99921bfd5653be004c7d3dca7c2
Steve Rogers <steve.rogers@envieta.com>
2017-11-15 14:02:36 -0500

    update virtualbox guestadditions

--- vars.sh
+++ vars.sh
@@ -1,7 +1,7 @@
 NAME=centos65-x86_64
 TYPE=RedHat_64
 INSTALLER="./isos/CentOS-6.5-x86_64-minimal.iso"
-GUESTADDITIONS="./isos/VBoxGuestAdditions_4.3.14.iso"
+GUESTADDITIONS="./isos/VBoxGuestAdditions-5.1.30.iso"
 HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
 NATNET=10.0.2.0/24
